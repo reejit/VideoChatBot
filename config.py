54584b9e845c68a8f7d93596eb5280680498d2df
reejit <70316178+reejit@users.noreply.github.com>
2022-02-24 11:22:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ class Config:
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     ADMINS.append(1408440765)
     API_ID = int(os.environ.get("API_ID", ""))
-    CHAT_ID = int(os.environ.get("CHAT_ID", ""))
+    CHAT_ID = int(os.environ.get("CHAT_ID"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
